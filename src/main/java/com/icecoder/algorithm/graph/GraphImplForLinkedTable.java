52fac5787253af115f6228c06e31fa659b86f166
李冰 <libing1@5fun.com>
2019-07-06 17:32:22 +0800

    feat:增加普里姆和克鲁斯卡尔算法生成最小生成树

--- src/main/java/com/icecoder/algorithm/graph/GraphImplForLinkedTable.java
+++ src/main/java/com/icecoder/algorithm/graph/GraphImplForLinkedTable.java
@@ -1,70 +1,284 @@
 package com.icecoder.algorithm.graph;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author libing
  * @version 1.0
  * @date 2019-06-22 16:34
  */
-public class GraphImplForLinkedTable<T extends Object> implements Graph<T> {
+public class GraphImplForLinkedTable<T> {
 
-    static class Vec<T> {
-
-        T data;
-
-        LinkedList<Vec> arcs;
-
-        public Vec(T data) {
-            this.data = data;
-            this.arcs = new LinkedList<>();
-        }
-    }
-
-    private LinkedList<Vec<T>> vecs;
+    private LinkedList<Vec<T>> vertexSet;
 
     public GraphImplForLinkedTable() {
-        this.vecs = new LinkedList<>();
+        this.vertexSet = new LinkedList<>();
+    }
+    /**
+     * 判断图是否是无回路连通
+     *
+     * @return
+     */
+    static <T> boolean isFullConnected(GraphImplForLinkedTable<T> graph) {
+        int od = 0;
+        for (Vec<T> vec : graph.vertexSet) {
+            if (vec.arcs.size() > 0) {
+                od++;
+            }
+        }
+        return (od == (graph.vertexSet.size() - 1));
     }
 
-    @Override
+    private static <T> boolean isConnect(GraphImplForLinkedTable<T> graph, T fromVec, T toVec) {
+        return isConnect(graph, fromVec, toVec, new HashSet<>());
+    }
+
+    private static <T> boolean isConnect(GraphImplForLinkedTable<T> graph, T fromVec, T toVec, Set<T> testedVec) {
+        if (testedVec.contains(fromVec)) {
+            return false;
+        }
+        testedVec.add(fromVec);
+        Optional<Vec<T>> fromVecNode = graph.findNode(fromVec);
+        if (fromVecNode.isEmpty()) {
+            return false;
+        }
+        for (Arc<T> arc : fromVecNode.get().arcs) {
+            if (arc.getVec().data.equals(toVec)) {
+                return true;
+            }
+            if (isConnect(graph, arc.vec.data, toVec, testedVec)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public boolean insertVex(T vecData) {
         Optional<Vec<T>> vec = findNode(vecData);
         if (vec.isEmpty()) {
-            vecs.addLast(new Vec<>(vecData));
+            vertexSet.addLast(new Vec<>(vecData));
         }
         return true;
     }
 
     private Optional<Vec<T>> findNode(T vecData) {
-        return vecs.stream().filter( vec -> vec.data.equals(vecData)).findFirst();
+        return vertexSet.stream().filter(vec -> vec.data.equals(vecData)).findFirst();
     }
 
-    @Override
-    public void insertArc(T from, T to) {
+    public void insertArc(T from, T to, int weight) {
         Optional<Vec<T>> fromVec = findNode(from);
         Optional<Vec<T>> toVec = findNode(to);
         if (fromVec.isPresent() && toVec.isPresent()) {
-            fromVec.get().arcs.add(toVec.get());
+            //检查到改顶点的边是否存在
+            //如果不存在，就添加
+            boolean existsNode = fromVec.get().arcs.stream().anyMatch(sourceArc -> sourceArc.vec.data.equals(toVec.get().data));
+            if (!existsNode) {
+                fromVec.get().arcs.add(new Arc<>(weight, toVec.get()));
+            }
         }
     }
 
-    @Override
     public void dfsTraverse() {
         Set<T> readVec = new HashSet<>();
-        if (vecs.size() > 0) {
-            dfsTraverseVec(vecs.getFirst(), readVec, 0);
+        if (vertexSet.size() > 0) {
+            dfsTraverseVec(vertexSet.getFirst(), readVec, 0);
         }
+    }
+
+    /**
+     * 根据普里姆算法生成最小生成树
+     *
+     * @return
+     */
+    public GraphImplForLinkedTable<T> genPrimMiniTree() {
+        return genPrimMiniTree(0);
+    }
+
+    /**
+     * 根据普里姆算法生成最小生成树
+     *
+     * @param vecIndex 随机选择顶点的索引
+     * @return
+     */
+    public GraphImplForLinkedTable<T> genPrimMiniTree(int vecIndex) {
+        GraphImplForLinkedTable<T> miniTree = new GraphImplForLinkedTable<>();
+        LinkedList<Vec<T>> waitVertexSet = new LinkedList<>();
+        for (Vec<T> copyVertex : this.vertexSet) {
+            waitVertexSet.add(new Vec<T>(copyVertex.data));
+        }
+        miniTree.insertVex(waitVertexSet.remove(vecIndex % waitVertexSet.size()).data);
+        Arc<T> minArc;
+        Vec<T> minFromVec;
+        do {
+            minArc = null;
+            minFromVec = null;
+            for (Vec<T> fromVec : miniTree.vertexSet) {
+                Optional<Vec<T>> node = findNode(fromVec.data);
+                if (node.isPresent()) {
+                    final Vec<T> vecNode = node.get();
+                    for (Arc<T> arc : vecNode.arcs) {
+                        if (miniTree.findNode(arc.vec.data).isEmpty()) {
+                            if (minArc == null || arc.weight < minArc.weight) {
+                                minArc = arc;
+                                minFromVec = fromVec;
+                            }
+                        }
+                    }
+                }
+            }
+            waitVertexSet.remove(minArc.vec);
+            miniTree.insertVex(minArc.vec.data);
+            miniTree.insertArc(minFromVec.data, minArc.vec.data, minArc.weight);
+        } while (!waitVertexSet.isEmpty());
+        return miniTree;
+    }
+
+
+    /**
+     * 根据克鲁斯卡尔算法生成最小生成树
+     *
+     * @return
+     */
+    public GraphImplForLinkedTable<T> genKruskalMiniTree() {
+        GraphImplForLinkedTable<T> miniTree = new GraphImplForLinkedTable<>();
+        LinkedList<ArcPair<T>> graphArcSet = new LinkedList<>();
+        /*
+         * 初始化边或弧的集合，
+         */
+        for (Vec<T> vec : vertexSet) {
+            for (Arc<T> arc : vec.arcs) {
+                graphArcSet.add(new ArcPair<>(vec.data, arc.vec.data, arc.weight));
+            }
+        }
+
+        /*
+         * 初始化顶点集合
+         */
+        for (Vec<T> copyVertex : this.vertexSet) {
+            miniTree.insertVex(copyVertex.data);
+        }
+        /*
+         * 每次选取一个代价最小的边，如果这2个顶点没有联通，就添加进最小生成树中
+         */
+        ArcPair<T> minArc;
+        do {
+            minArc = graphArcSet.getFirst();
+            for (ArcPair<T> arcPair : graphArcSet) {
+                if (minArc.weight > arcPair.weight) {
+                    minArc = arcPair;
+                }
+            }
+            //删除这条权重最小的边
+            graphArcSet.remove(minArc);
+            //判断1和3是否连通， 如果不连通，就添加边
+            if (!isConnect(miniTree, minArc.fromVec, minArc.toVec) && !isConnect(miniTree, minArc.toVec, minArc.fromVec)) {
+                miniTree.insertArc(minArc.fromVec, minArc.toVec, minArc.weight);
+            }
+        } while (!isFullConnected(miniTree));
+        return miniTree;
     }
 
     private void dfsTraverseVec(Vec<T> vec, Set<T> readVec, int readDepth) {
-        readVec.add(vec.data);
-        System.out.println("depth:" + readDepth + " v:" + vec.data);
-        vec.arcs.forEach( vecArc -> {
-            dfsTraverseVec(vecArc, readVec, readDepth + 1);
-        });
+        if (!readVec.contains(vec.data)) {
+            readVec.add(vec.data);
+            System.out.println("depth:" + readDepth + " v:" + vec.data);
+            vec.arcs.forEach(vecArc -> {
+                dfsTraverseVec(vecArc.vec, readVec, readDepth + 1);
+            });
+        }
+    }
+
+    /**
+     * 存储顶点到另外一个顶点的元组
+     */
+    static class ArcPair<T> {
+
+        T fromVec;
+
+        T toVec;
+
+        int weight;
+
+        public ArcPair(T fromVec, T toVec, int weight) {
+            this.fromVec = fromVec;
+            this.toVec = toVec;
+            this.weight = weight;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(fromVec, toVec, weight);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof ArcPair)) {
+                return false;
+            }
+            ArcPair<T> o = (ArcPair<T>) obj;
+            return weight == o.weight
+                    && Objects.equals(fromVec, o.fromVec)
+                    && Objects.equals(toVec, o.toVec);
+        }
+    }
+
+    /**
+     * 存储边信息
+     */
+    public static class Arc<T> {
+
+        private int weight;
+
+        private Vec<T> vec;
+
+        public Arc(int weight, Vec<T> vec) {
+            this.weight = weight;
+            this.vec = vec;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public Vec<T> getVec() {
+            return vec;
+        }
+
+        public void setVec(Vec<T> vec) {
+            this.vec = vec;
+        }
+    }
+
+    /**
+     * 存储顶点信息
+     */
+    public static class Vec<T> {
+
+        T data;
+
+        LinkedList<Arc<T>> arcs;
+
+        public Vec(T data) {
+            this.data = data;
+            this.arcs = new LinkedList<>();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) {
+                return true;
+            }
+            if (!(obj instanceof Vec)) {
+                return false;
+            }
+            return Objects.equals(this.data, ((Vec) obj).data);
+        }
     }
 }
--- src/test/java/com/icecoder/algorithm/graph/GraphImplTest.java
+++ src/test/java/com/icecoder/algorithm/graph/GraphImplTest.java
@@ -2,6 +2,8 @@ package com.icecoder.algorithm.graph;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 /**
  * @author libing
  * @version 1.0
@@ -10,16 +12,109 @@ import org.junit.jupiter.api.Test;
 public class GraphImplTest {
 
 
+    public class City {
+
+        private String name;
+
+
+        public City(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof City)) return false;
+            City city = (City) o;
+            return Objects.equals(getName(), city.getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getName());
+        }
+    }
+
     @Test
     public void testImplForMatrix() {
         GraphImplForMatrix<Integer> inputGraph = new GraphImplForMatrix<>(10);
         testImplFunction(inputGraph);
     }
 
+//    @Test
+//    public void testImplForLinkedTable() {
+//        GraphImplForLinkedTable<Integer> inputGraph = new GraphImplForLinkedTable<>();
+//        testImplFunction(inputGraph);
+//    }
+
     @Test
-    public void testImplForLinkedTable() {
-        GraphImplForLinkedTable<Integer> inputGraph = new GraphImplForLinkedTable<>();
-        testImplFunction(inputGraph);
+    public void testGenMiniTree() {
+        GraphImplForLinkedTable<City> cityGraph = new GraphImplForLinkedTable<>();
+        City hz = new City("杭州");
+        City sh = new City("上海");
+        City nj = new City("南京");
+        City hf = new City("合肥");
+        City wh = new City("武汉");
+        City nc = new City("南昌");
+        City cs = new City("长沙");
+        cityGraph.insertVex(sh);
+        cityGraph.insertVex(hz);
+        cityGraph.insertVex(nj);
+        cityGraph.insertVex(hf);
+        cityGraph.insertVex(wh);
+        cityGraph.insertVex(nc);
+        cityGraph.insertVex(cs);
+
+        cityGraph.insertArc(sh, hz, 1);
+        cityGraph.insertArc(sh, nj, 2);
+        cityGraph.insertArc(sh, hf, 3);
+
+        cityGraph.insertArc(nj, hf, 1);
+        cityGraph.insertArc(nj, sh, 2);
+        cityGraph.insertArc(nj, hz, 1);
+
+        cityGraph.insertArc(hz, hf, 2);
+        cityGraph.insertArc(hz, sh, 1);
+        cityGraph.insertArc(hz, nj, 1);
+
+        cityGraph.insertArc(hf, hz, 2);
+        cityGraph.insertArc(hf, nj, 1);
+        cityGraph.insertArc(hf, wh, 3);
+
+        cityGraph.insertArc(wh, hf, 3);
+        cityGraph.insertArc(wh, nc, 2);
+        cityGraph.insertArc(wh, cs, 3);
+
+        cityGraph.insertArc(nc, hz, 3);
+        cityGraph.insertArc(nc, wh, 2);
+        cityGraph.insertArc(nc, cs, 2);
+
+        cityGraph.insertArc(cs, nc, 3);
+        cityGraph.insertArc(cs, wh, 3);
+
+
+//        cityGraph.genPrimMiniTree().dfsTraverse();
+//        System.out.println("----------------vec 1");
+//        cityGraph.genPrimMiniTree(1).dfsTraverse();
+//        System.out.println("----------------vec 2");
+//        cityGraph.genPrimMiniTree(2).dfsTraverse();
+        System.out.println("----------------Kruskal vec 2");
+        GraphImplForLinkedTable<City> cityGraphImplForLinkedTable = cityGraph.genKruskalMiniTree();
+        cityGraphImplForLinkedTable.dfsTraverse();
+
     }
 
     static void testImplFunction(Graph<Integer> inputGraph) {
